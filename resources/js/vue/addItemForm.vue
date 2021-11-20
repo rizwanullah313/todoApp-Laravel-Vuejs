739515b61f341a4379c72981b5e22198bfe773d9
rizwanullah313 <ru323091@gmail.com>
2021-11-20 18:14:31 +0000

    succesfully work plus button with list view items

--- resources/js/vue/addItemForm.vue
+++ resources/js/vue/addItemForm.vue
@@ -3,6 +3,7 @@
         <input type="text" v-model="item.name" />
         <font-awesome-icon
              icon="plus-square"
+             @click="addItem()"
              :class="[ item.name ? 'active' : 'inactive', 'plus']"
         />
     </div>
@@ -16,6 +17,24 @@ export default{
            item: {
                name: ""
            }
+       }
+   },
+   methods: {
+       addItem(){
+           if(this.item.name == ''){
+               return;
+           }
+
+           axios.post('api/item/store', {
+               item: this.item
+           }).then(response => {
+               if( response.status == 201){
+                   this.item.name == "";
+               }
+           })
+           .catch( error => {
+               console.log(error);
+           })
        }
    }
 }
--- resources/js/vue/listView.vue
+++ resources/js/vue/listView.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
-        List View
+        <div v-for="(item, index) in items" :key="index">
+          <list-item
+          :item="item"
+          class="item"
+          />
+
+        </div>
     </div>
 </template>
 
 <script>
-
+import  listItem from "./listItem.vue"
 export default{
-
+     props: ['items'],
+     components: {
+         listItem
+     }
 }
 </script>
+
+<style scoped>
+.item{
+    background: #e6e6e6;
+    padding: 5px;
+    margin-top: 5px;
+}
+</style>
